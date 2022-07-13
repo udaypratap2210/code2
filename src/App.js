8b224ab23d4a82b1eea63ad246f3f186d5b50b1d
Uday Pratap <udaysenger2210@gmail.com>
2022-07-13 10:06:43 +0530

    Blackpiano Assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CustomizedTables from "../src/Table";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  let call = async () => {
+    try {
+      const res = await fetch("https://dummyjson.com/products");
+      const response = await res.json();
+      setData(response.products);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    call();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CustomizedTables data={data} />
     </div>
   );
 }
---
+++ src/Table.js
@@ -0,0 +1,69 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
+function CustomizedTables(props) {
+  console.log(styled);
+
+  return (
+    <TableContainer>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Brand</StyledTableCell>
+            <StyledTableCell>Description</StyledTableCell>
+            <StyledTableCell>Category</StyledTableCell>
+            <StyledTableCell>Discount(%)</StyledTableCell>
+            <StyledTableCell>Price</StyledTableCell>
+            <StyledTableCell>Rating</StyledTableCell>
+            <StyledTableCell>Stock</StyledTableCell>
+            <StyledTableCell>Title</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.data.map((row) => (
+            <StyledTableRow key={row.id}>
+              <StyledTableCell component="th" scope="row">
+                {row.brand}
+              </StyledTableCell>
+              <StyledTableCell>{row.description}</StyledTableCell>
+              <StyledTableCell>{row.category}</StyledTableCell>
+              <StyledTableCell>{row.discountPercentage}</StyledTableCell>
+              <StyledTableCell>{row.price}</StyledTableCell>
+              <StyledTableCell>{row.rating}</StyledTableCell>
+              <StyledTableCell>{row.stock}</StyledTableCell>
+              <StyledTableCell>{row.title}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+export default CustomizedTables;
